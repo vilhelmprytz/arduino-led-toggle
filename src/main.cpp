661e1c5e2fbbf5a394f821adff131fba41d4e61b
Vilhelm Prytz <vilhelm@prytznet.se>
2020-09-11 13:40:11 +0200

    Initial application

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
 
+const int ledPin = 13; // pin that the LED is connected to
+const int buttonPin = 2; // pin that the button is connected to
+
+int buttonState = 0; // current state of button
+
 void setup() {
-  // put your setup code here, to run once:
+  // I/O
+  pinMode(ledPin, OUTPUT); // set LED as output (O)
+  pinMode(buttonPin, INPUT); // set button as input (I)
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // read current state
+  buttonState = digitalRead(buttonPin);
+
+  // if the button is pressed (e.g. HIGH, 1)
+  if (buttonState == HIGH) {
+    // turn LED on
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off
+    digitalWrite(ledPin, LOW);
+  }
 }
